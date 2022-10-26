d8f6d2c76942ffedf1876540b1e2a3d6961adaaf
Julio Arruda <julio.arruda@outlook.com>
2022-10-26 00:04:38 +0000

    change staging check

--- swap.sh
+++ swap.sh
@@ -3,6 +3,8 @@
 APP_NAME=$1
 echo "App Name: $APP_NAME"
 
+APP_VERSION=$2
+echo "App Version: $APP_VERSION"
 
 # define slot names
 SLOT_BLUE="blue"
@@ -10,37 +12,49 @@ SLOT_GREEN="green"
 
 # Initial Version
 APP_VERSION=0
+NAMESPACE_ENVIRONMENT="staging"
  
 
 # check version of deployment in slot
 function check_slot_version() {
     #echo "Checking version of $1 Slot"
     VERSION=$(kubectl get deployment -n $1 $APP_NAME -o jsonpath='{.metadata.labels.release}')
+    NAMESPACE_ENV=$(kubectl get namespace $1 -o jsonpath='{.metadata.labels.environment}')
     #echo "Version: $VERSION"
     APP_VERSION=$VERSION
+    NAMESPACE_ENVIRONMENT=$NAMESPACE_ENV
 }
 
 check_slot_version $SLOT_BLUE
 BLUE_VERSION=$(echo $APP_VERSION | tr -d '.')
+BLUE_ENVIRONMENT=$NAMESPACE_ENVIRONMENT
 
 check_slot_version $SLOT_GREEN
 GREEN_VERSION=$(echo $APP_VERSION | tr -d '.')
+GREEN_ENVIRONMENT=$NAMESPACE_ENVIRONMENT
 
 echo "Blue Version: $BLUE_VERSION"
 echo "Green Version: $GREEN_VERSION"
+echo "Blue Environment: $BLUE_ENVIRONMENT"
+echo "Green Environment: $GREEN_ENVIRONMENT"
 
 
- echo $GREEN_VERSION | tr -d '.'
  
-if [ $GREEN_VERSION -gt $BLUE_VERSION  ]; then 
-    echo "Green is higher than blue - swapping to green"
+if [[ $GREEN_ENVIRONMENT == "staging" ]]; then  
+    echo "Green is staging - swapping to prod"
     kubectl apply -f ingress.yml -n $SLOT_GREEN
-    kubectl apply -f ingress-blue.yml -n $SLOT_BLUE
+    kubectl label --overwrite namespace $SLOT_GREEN environment=prod 
     
-else
-    echo "Blue is higher than green - swapping to blue"
+    kubectl apply -f ingress-blue.yml -n $SLOT_BLUE
+    kubectl label --overwrite namespace $SLOT_BLUE environment=staging 
+    
+elif [[ $BLUE_ENVIRONMENT == "staging" ]]; then
+    echo "Blue is staging - swapping to prod"
     kubectl apply -f ingress.yml -n $SLOT_BLUE
+    kubectl label --overwrite namespace $SLOT_BLUE environment=prod
+
     kubectl apply -f ingress-blue.yml -n $SLOT_GREEN
+    kubectl label --overwrite namespace $SLOT_GREEN environment=staging
 fi
 
 
